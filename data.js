5abcc064521dcb08a457a15a5357b4f63b755e6e
JOMANA1980000000000000000000000000000 <134141607+JOMANA1980000000000000000000000000000@users.noreply.github.com>
2023-05-20 14:43:02 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+export let plansList = [
+	{
+		//####################################### start updating as needed #######################################
+		name: "normal",
+		monthlySubPrice: 20,
+		yearlySubPrice: 200,
+		players: 4,
+		icon: 10,
+		ads: false,
+		ultimateteam: false,
+		customerCare: false
+	},
+	{
+		name: "pro",
+		monthlSubPrice: 30,
+		yearlySubPrice: 100,
+		icon:40,
+ultimateteat : false,
+		customerCare: "By Email"
+	},
+	{
+		name: "premium",
+		monthlySubPrice: 50,
+		yearlySubPrice:240,
+		players: 10,
+		icon:23,: true,
+		ads: true,
+		customerCare: "24/7 Line"
+		//#######################################      stop  updating      #######################################
